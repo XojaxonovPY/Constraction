ca42cde29b6f8d47f1febefaaf469f4e962caa1f
User <asqarservis00001@gmail.com>
2025-10-09 21:23:56 +0500

    updated

--- tasks.py
+++ tasks.py
@@ -1,22 +1,29 @@
 from invoke import task
 
+
 @task
 def mig(c):
     c.run("python manage.py makemigrations")
+
 
 @task
 def upg(c):
     c.run("python manage.py migrate")
 
+
 @task
 def admin(c):
     c.run("python manage.py createsuperuser")
+
 
 @task
 def apps(c):
     c.run("python manage.py startapp apps")
 
 
+@task
+def statics(c):
+    c.run("python manage.py collectstatic")
 
 
 @task
